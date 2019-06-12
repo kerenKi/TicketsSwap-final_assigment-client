7004afc6d9ab3a7942b2c47416507206dc9689d8
kerenKi <kkinberg13@gmail.com>
2019-06-12 15:24:35 +0200

    init structure, connect to browser router and store

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+// import events from './events'
+// import event from './singleEvent'
+// import currentUser from './currentUser'
+
+export default combineReducers ({
+    // events,
+    // event,
+    // currentUser
+})
